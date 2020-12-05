54138c43f3b28d39e0b04eb82b8ba6f464e7df20
vinaydawani <vdawani6@gmail.com>
2020-12-05 06:19:38 -0500

    front page changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
-import Typed from 'typed';
+window.addEventListener("load", () => {
+    Particles.init({
+        selector: '.particle-bg',
+        maxParticles: 75,
+        color: "#f2f2f2",
+        connectParticles: true,
+        speed: 0.4,
+        minDistance: 120,
+        responsive: [
+            {
+                breakpoint: 576,
+                options: {
+                    maxParticles: 50,
+                    color: '#f2f2f2',
+                    minDistance: 100,
+                }
+            }
+        ]
+    });
+});
 
-var typed = new Typed("#typing", {
-    strings: ['<strong>Vinay Dawani.</strong>', '<strong>A Designer.</strong>', '<strong>A thinker.</strong>'],
-
+window.addEventListener("load", () => {
+    var typed = new Typed("#typing", {
+        strings: ["Vinay Dawani.", "A Designer.", "A Developer."],
+        typeSpeed: 100,
+        startDelay: 1000,
+        backSpeed: 80,
+        backDelay: 500,
+        loop: true,
+        smartBackspace: false,
+      });
 })
-
-// var typed = require('typed.js');
-
-// var asd = new typed.Typed("#typing", {
-//     strings: ['<strong>Vinay Dawani.</strong>', '<strong>A Designer.</strong>', '<strong>A thinker.</strong>'],
-//     typeSpeed: 40
-// })
