b96c18626be8bc4f0ffe738f857c161355a17bf5
vinaydawani <vdawani6@gmail.com>
2020-12-04 19:41:18 -0500

    type ani

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import Typed from 'typed';
+
+var typed = new Typed("#typing", {
+    strings: ['<strong>Vinay Dawani.</strong>', '<strong>A Designer.</strong>', '<strong>A thinker.</strong>'],
+
+})
+
+// var typed = require('typed.js');
+
+// var asd = new typed.Typed("#typing", {
+//     strings: ['<strong>Vinay Dawani.</strong>', '<strong>A Designer.</strong>', '<strong>A thinker.</strong>'],
+//     typeSpeed: 40
+// })
